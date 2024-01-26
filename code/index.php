61dacb8eac883cb4aed66dfe205013c148963ed3
AntonyprakashAP <antonyprakash1612@gmail.com>
2024-01-26 13:44:31 +0530

    project adding

---
+++ code/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="./assets/img/icon.png">
+    <title>Dairy</title>
+    <link rel="stylesheet" href="./css/sty.css">
+</head>
+<body>
+
+    <div class="main">
+        <div class="navbar">
+            <div class="img"> <img src="./assets/img/icon.png" alt=""></div>
+            <div class="icon">
+                <h2 class="logo" style="color:white">Dairy</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                </ul>
+            </div>
+        </div> 
+        <div class="content">
+            <h1>WE CAN USE THE <br><span>DIARY</span> <br>FOR OUR DAILY UPDATE</h1>
+            <p class="par">"One advantage in keeping a diary is that you become aware <br> with reassuring clarity of the changes which you constantly suffer." 
+                <br>~ Franz Kafka</p>
+
+                
+
+                <div class="form">
+                    <button class="btnn"><a href="login.php">Login</a></button>
+                    <button class="btnn"><a href="registration.php">Register</a></button>
+
+                    <div class="icons">
+                        <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                    </div>
+
+                </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</body>
+</html>
